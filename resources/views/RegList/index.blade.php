2b0f3e0361d5af27307537d5d31577cb87975507
tingkn <tkn010119@gmail.com>
2023-07-07 20:34:07 +0800

    Add function and make minor modifications

--- resources/views/RegList/index.blade.php
+++ resources/views/RegList/index.blade.php
@@ -23,7 +23,7 @@
         @endif
 
         <form class="search" action="{{ route('reglist.index') }}" method="GET">
-            <input type="text" class="form-control" id="search" name="search" placeholder="Search by name and ID">
+            <input type="text" class="form-control" id="search" name="search" placeholder="Search by name or ID">
         </form>
         
         <table>
@@ -37,31 +37,40 @@
                         <a class="sort-link" href="{{ route('reglist.index', ['sort' => 'not-eligible']) }}">&#x2193;</a>
                     </th>
                     <th>Purchase Status</th>
-                    <th>Loan Amount</th>
+                    <th>Loan Amount (RM)</th>
                     <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
-                @foreach($registrations as $registration)
-                <tr>
-                    <td>{{ $registration->id }}</td>
-                    <td>{{ $registration->name }}</td>
-                    <td>{{ $registration->down_payment }}</td>
-                    <td class="{{ $registration->eligibility ? 'eligible' : 'not-eligible' }}">
-                        {{ $registration->eligibility ? 'Eligible' : 'Not Eligible' }}
-                    </td>
-                    <td>{{ $registration->purchase_status }}</td>
-                    <td>{{ $registration->loan_amount }}</td>
-                    <td>
-                        <a class="sort-link" href="{{ route('reglist.edit',$registration->id) }}">View Details</a>
-                    </td>
-                </tr>
-                @endforeach
+                @if($registrations && $registrations->count() > 0)
+                    @foreach($registrations as $registration)
+                    <tr>
+                        <td>{{ $registration->id }}</td>
+                        <td>{{ $registration->name }}</td>
+                        <td>{{ $registration->down_payment }}</td>
+                        <td class="{{ $registration->eligibility ? 'eligible' : 'not-eligible' }}">
+                            {{ $registration->eligibility ? 'Eligible' : 'Not Eligible' }}
+                        </td>
+                        <td>{{ $registration->purchase_status }}</td>
+                        <td>{{ $registration->loan_amount }}</td>
+                        <td>
+                            <a class="sort-link" href="{{ route('reglist.edit',$registration->id) }}">View Details</a>
+                        </td>
+                    </tr>
+                    @endforeach
+                @else
+                    <tr>
+                        <td colspan="7">No registrations found.</td>
+                    </tr>
+                @endif
             </tbody>
         </table>
         <div class="pagination-container">
             {{ $registrations->links() }}
         </div>
     </div>
+    <footer>
+        <p>&copy; 2023 CapBay Auto. All rights reserved.</p>
+    </footer>
 </body>
 </html>
